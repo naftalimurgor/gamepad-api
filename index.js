a5d9c77e31f55fea1f1a65a1a77d8bdfbeaa0fd9
naftalimurgor <murgornaftali@gmail.com>
2021-11-01 19:06:06 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener("gamepadconnected", function(e) {
+    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+      e.gamepad.index, e.gamepad.id,
+      e.gamepad.buttons.length, e.gamepad.axes.length);
+  });
+  
